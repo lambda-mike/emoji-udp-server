57bf63f6940ceba2e501e3c4720e038871beac72
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-07-10 17:23:21 +0100

    Parse cmd-line args

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
-// TODO use flags to parse cmd arguments
-
 import (
+	"flag"
 	"fmt"
 	"github.com/emoji-udp-server/config"
 	"log"
 )
 
 func main() {
-	isRaw := false
-	n := 1
-	sep := ""
-	conf, err := config.Create(isRaw, n, sep)
+	isRaw := flag.Bool("r", false, "disable the translation from keyword to emoji")
+	n := flag.Int("n", 1, "cmd input number multiplier")
+	sep := flag.String("s", "", "emojis separator")
+	flag.Parse()
+	conf, err := config.Create(*isRaw, *n, *sep)
 	if err != nil {
-		log.Panicln("Could not create Config struct, got error: ", err)
+		log.Panicln("ERR: Could not create Config struct from command-line arguments, got error: ", err)
 	}
 	fmt.Println("Welcome to Emoji UDP Server!")
 	log.Println("INFO: Config: ", conf)
