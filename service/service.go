86329f31cbe2d2b929958230dfc9c96735b7be26
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-07-12 10:23:39 +0100

    Execute transformers in Handle method

--- service/service.go
+++ service/service.go
@@ -11,8 +11,9 @@ import (
 )
 
 type EmojiService struct {
-	ui     contracts.UI
-	parser contracts.CmdParser
+	ui           contracts.UI
+	parser       contracts.CmdParser
+	transformers []contracts.CmdTransformer
 }
 
 func (es *EmojiService) Handle(req string) {
@@ -20,6 +21,8 @@ func (es *EmojiService) Handle(req string) {
 	command, err := es.parseRequest(req)
 	log.Println("INFO cmd", command, "err", err)
 	log.Println("TODO transform cmd (2 transformers composed)")
+	cmdTran, err := es.transformCmd(command, err)
+	log.Println("INFO cmd", cmdTran, "err", err)
 	log.Println("TODO build response")
 	log.Println("TODO print response")
 	es.ui.Print(req)
@@ -33,11 +36,28 @@ func Create(
 	es := EmojiService{}
 	es.ui = ui
 	es.parser = cmd.CreateParser()
+	transformers := make([]contracts.CmdTransformer, 0, 2)
+	transformers = append(transformers, cmd.CreateMultiplier(cfg.N))
+	transformers = append(transformers, cmd.CreateTranslator(cfg.Raw))
+	es.transformers = transformers
 	return &es
 }
 
 func (es *EmojiService) parseRequest(req string) (contracts.Cmd, error) {
 	return es.parser.Parse(req)
+}
+
+func (es *EmojiService) transformCmd(c contracts.Cmd, err error) (contracts.Cmd, error) {
+	if err != nil {
+		return c, err
+	}
+	for _, t := range es.transformers {
+		c, err = t.Transform(c)
+		if err != nil {
+			break
+		}
+	}
+	return c, err
 }
 
 type EmojiConcatenator struct {
@@ -51,9 +71,11 @@ func (e *EmojiConcatenator) Build(cmd contracts.Cmd) string {
 	if cmd.N < 0 {
 		return ""
 	}
+	// emojis will contain extra sep at the end
 	emojis := strings.Repeat(cmd.Emoji+e.separator, cmd.N)
 	if cmd.N > 0 {
 		limit := len(emojis) - len(e.separator)
+		// Remove extra separator at the end
 		emojis = emojis[:limit]
 	}
 	return emojis
