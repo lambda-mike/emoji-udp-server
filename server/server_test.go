d50a536f7d60ad12539c4b041d4f5078c25ba8d3
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-07-12 17:11:52 +0100

    Add server e2e test

---
+++ server/server_test.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	"net"
+	"testing"
+)
+
+type mockHandler struct {
+	state chan string
+}
+
+func (h *mockHandler) Handle(s string) {
+	h.state <- s
+}
+
+func TestListen(t *testing.T) {
+	t.Log("Listen")
+	{
+		t.Log("Given new server with mocked handler")
+		{
+			t.Log("When called with some msg")
+			{
+				port := 57777
+				msg := "testing message"
+				handler := mockHandler{}
+				handler.state = make(chan string)
+				sut := CreateUDPServer(port, &handler)
+				go sut.Listen()
+
+				udpAddr := net.UDPAddr{IP: []byte{127, 0, 0, 1}, Port: port, Zone: ""}
+				conn, err := net.DialUDP("udp", nil /*(laddr)*/, &udpAddr)
+				defer conn.Close()
+				if err != nil {
+					t.Fatal("Could not establish connection to server", err)
+				}
+				conn.Write([]byte(msg))
+				state := <-handler.state
+				if state != msg {
+					t.Fatal("Handler did not receive proper msg! Got:", state, "should be:", msg)
+				}
+				t.Log("Handler should receive proper message in its state")
+			}
+		}
+	}
+}
