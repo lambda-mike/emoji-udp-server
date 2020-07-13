3748c4e229479f5ef0c975e281f24803df6142bf
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-07-13 18:29:37 +0100

    Add logging to metrics service

--- src/metrics/metrics.go
+++ src/metrics/metrics.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"fmt"
+	"log"
 	"sync"
 )
 
@@ -12,6 +13,7 @@ type MetricsService struct {
 }
 
 func (m *MetricsService) GetReport() string {
+	log.Println("INFO GetReport")
 	m.mtx.RLock()
 	template := "Valid inputs: %d\nInvalid inputs: %d"
 	report := fmt.Sprintf(template, m.valid, m.invalid)
@@ -20,17 +22,20 @@ func (m *MetricsService) GetReport() string {
 }
 
 func (m *MetricsService) IncValid() {
+	log.Println("INFO IncValid")
 	m.mtx.Lock()
 	m.valid++
 	m.mtx.Unlock()
 }
 
 func (m *MetricsService) IncInvalid() {
+	log.Println("INFO IncInvalid")
 	m.mtx.Lock()
 	m.invalid++
 	m.mtx.Unlock()
 }
 
 func Create() *MetricsService {
+	log.Println("INFO MetricsService Create")
 	return &MetricsService{}
 }
