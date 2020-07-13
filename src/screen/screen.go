1657b146b3571666dbadece22feef2acfc418706
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-07-13 17:41:14 +0100

    Add mutex to screen

--- src/screen/screen.go
+++ src/screen/screen.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"github.com/emoji-udp-server/contracts"
 	"log"
+	"sync"
 )
 
-type Screen struct{}
+type Screen struct {
+	mtx sync.Mutex
+}
 
 func (r *Screen) Print(cmd string) {
+	r.mtx.Lock()
 	fmt.Println(cmd)
+	r.mtx.Unlock()
 }
 
 func Create() contracts.UI {
